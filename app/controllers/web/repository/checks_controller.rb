97641acdcf6c047a8bd91de00f0a0e1a5201e1fe
Yura <yura.maksimov.04@mail.ru>
2025-04-30 17:52:21 +0300

    Линтинг + получение commit_id для проверки

--- app/controllers/web/repository/checks_controller.rb
+++ app/controllers/web/repository/checks_controller.rb
@@ -2,13 +2,22 @@ module Web
     module Repository
         class ChecksController < Web::ApplicationController
             before_action :authenticate_user!
+            before_action :set_repository
 
             def create
-                
+                check = @repository.checks.build()
+                linter = ApplicationContainer[:linter].new(@repository.clone_url, check)
+                linter.call
             end
 
             def show
 
+            end
+
+            private
+
+            def set_repository
+                @repository = ::Repository.find(params[:repository_id])
             end
 
         end
--- app/services/linter_service.rb
+++ app/services/linter_service.rb
@@ -4,15 +4,16 @@ class LinterService
 
     def initialize(clone_url, check, output_dir: '/tmp')
         @clone_url = clone_url
-        @check = repository_check
+        @check = check
         @output_dir = output_dir
-        generate_temp_dir
+        @temp_dir = generate_temp_dir
     end
 
     def call
         begin
             clone_repo
             run_linter
+            cleanup
             @check.complete!
         rescue StandardError => e
             @check.fail!
@@ -25,11 +26,17 @@ class LinterService
         @check.clone!
         clone_path = "#{@output_dir}/#{@temp_dir}"
 
-        cmd = "git clone #{clone_url} #{clone_path}"
+        cmd = "git clone #{@clone_url} #{clone_path}"
         _, stderr, status = Open3.capture3(cmd)
 
-        if !status.success
+        if !status.success?
             raise "Failed to clone repository!"
+        end
+
+        Dir.chdir(clone_path) do
+            cmd_commit_id = 'git rev-parse HEAD'
+            commit_id, stderr, status = Open3.capture3(cmd_commit_id)
+            @check.update!(commit_id: commit_id.chomp)
         end
     end
 
@@ -38,18 +45,24 @@ class LinterService
         repo_path = "#{@output_dir}/#{@temp_dir}"
         rubocop_config = Rails.root.join('.rubocop.yml').to_s
 
-        cmd = "bundle exec rubocop --config #{rubocop_config} #{repo_path}"
-        stdout, stderr, status = Open3.capture3(cmd)
-
-        debugger
-
-        if !status.success
-            raise "Failed to clone repository!"
+        begin
+            cmd = "bundle exec rubocop --config #{rubocop_config} #{repo_path}"
+            stdout, stderr, status = Open3.capture3(cmd)
+        rescue StandardError => e
+            @check.fail!
         end
+        
     end
 
+    private
+
     def generate_temp_dir
-        @temp_dir = SecureRandom.hex(8)
+        SecureRandom.hex(8)
+    end
+
+    def cleanup
+        temp_path = "#{@output_dir}/#{@temp_dir}"
+        FileUtils.rm_rf(temp_path) if Dir.exist?(temp_path)
     end
 
 end
