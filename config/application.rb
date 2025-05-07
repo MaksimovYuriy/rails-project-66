6a681f8bac3ce9084b0a41533dfb3201cb52f448
Yura <yura.maksimov.04@mail.ru>
2025-05-07 14:03:13 +0300

    Локализация (без писем)

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,7 @@ module RailsProject66
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.available_locales = [:en, :ru]
+    config.i18n.default_locale = :en
   end
 end
