3a10a1de155e17d1baf5862b1ef35f3c4ba81202
Yura <yura.maksimov.04@mail.ru>
2025-05-09 11:00:12 +0300

    Правки линтера

--- app/policies/web/repository/check_policy.rb
+++ app/policies/web/repository/check_policy.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 module Web
-    module Repository
-        class CheckPolicy < ApplicationPolicy
-            def show?
-                record.repository.user_id == user.id
-            end
-        end
+  module Repository
+    class CheckPolicy < ApplicationPolicy
+      def show?
+        record.repository.user_id == user.id
+      end
     end
+  end
 end
--- app/policies/web/repository_policy.rb
+++ app/policies/web/repository_policy.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 module Web
-    class RepositoryPolicy < ApplicationPolicy
-        def show?
-            record.user_id == user.id
-        end
+  class RepositoryPolicy < ApplicationPolicy
+    def show?
+      record.user_id == user.id
     end
+  end
 end
