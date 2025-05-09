aae601120c5a0100a6b4b91eaaf143a6b49f70f3
Yura <yura.maksimov.04@mail.ru>
2025-05-09 19:04:11 +0300

    Вынесение API за предел Web

--- config/routes.rb
+++ config/routes.rb
@@ -14,10 +14,10 @@ Rails.application.routes.draw do
         resources :checks, only: %i[create show]
       end
     end
+  end
 
-    namespace :api do
-      resources :checks, only: %i[create]
-    end
+  namespace :api do
+    resources :checks, only: %i[create]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
