35d49f583788c3129dbddf99ca663b5b35edc601
Yura <yura.maksimov.04@mail.ru>
2025-05-10 23:04:45 +0300

    Обновление /api/checks

--- app/controllers/api/checks_controller.rb
+++ app/controllers/api/checks_controller.rb
@@ -6,16 +6,15 @@ module Api
 
     def create
 
-      if request.format.json? || request.headers['X-GitHub-Event'].present?
+      if request.headers['X-GitHub-Event'].present?
         event = request.headers['X-GitHub-Event']
         return head :bad_request unless event == 'push'
-
         payload = JSON.parse(request.body.read)
         github_id = payload.dig('repository', 'id')
       else
+        # Обработка обычных HTML/json-запросов без заголовка GitHub
         github_id = params.dig(:repository, :id)
       end
-
 
       repository = ::Repository.find_by(github_id: github_id)
 
