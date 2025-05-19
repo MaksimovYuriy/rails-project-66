8d35ca8b3e6a32fcbc4447eee1ebdf0981494778
Yura <yura.maksimov.04@mail.ru>
2025-05-19 13:17:27 +0300

    Тесты на logout и failure

--- test/controllers/web/auth_controller_test.rb
+++ test/controllers/web/auth_controller_test.rb
@@ -2,6 +2,11 @@
 
 module Web
   class AuthControllerTest < ActionDispatch::IntegrationTest
+
+    setup do
+      @user = users(:user)
+    end
+
     test 'guthub auth page' do
       post auth_request_path('github')
 
@@ -30,6 +35,23 @@ module Web
 
       assert user
       assert signed_in?
+    end
+
+    test 'should logout' do
+      sign_in @user
+
+      delete auth_logout_path
+
+      assert_nil current_user
+    end
+
+    test 'login failure' do
+      sign_in @user
+
+      get auth_failure_path
+
+      assert_response :redirect
+      assert_nil current_user
     end
   end
 end
